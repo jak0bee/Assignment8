900312df3c4bfc1038f321176fc57acb41d65df1
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-06-02 19:48:45 +0200

    wszystkie roll testy

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class GameTest {
     Game game;
@@ -17,21 +18,28 @@ public class GameTest {
 
     @Test
     public void normalRollTest() {
+        int startingPostition = game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace();
         game.roll(3);
-        assertEquals(game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace(),3);
+        assertEquals(game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace(),startingPostition+3);
+        startingPostition +=3;
         game.roll(2);
-        assertEquals(game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace(),5);
-        game.roll(0);
-        assertEquals(game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace(),5);
-
+        assertEquals(game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace(),startingPostition+ 2);
 
     }
 
     @Test
     public void negativeRollTest(){
-        game.roll(-1);
+        assertThrows(RuntimeException.class, () -> {
+            game.roll(-1);
+        });
     }
-
+    @Test
+    public void zeroRollTest(){
+        int startingPostition = game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace();
+        game.roll(0);
+        assertEquals(game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace(),startingPostition);
+    }
+    @Test
     public void overflowRollTest(){
         game.roll(999);
         assert(game.getPLAYERS().get(game.getCurrentPlayerIndex()).getPlace()<12);
